e9f339dda012e928dacacf8dd43a6345cf87cd51
Kevin Wong <kevingywong@gmail.com>
2021-11-08 13:11:11 -0500

    implemented a promise

--- app.js
+++ app.js
@@ -2,3 +2,17 @@ console.log("Hello World!\n==========\n");
 
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
+let isWatching = new Promise((resolve, reject) => {
+    let userWatchingLiveStream = true;
+    if (userWatchingLiveStream == true) {
+        resolve("Thumbs up and Subscribe!");
+    } else {
+        reject("User left.");
+    }
+});
+
+isWatching.then((message) => {
+    console.log('This is in the then : ' + message)
+}).catch((message) => {
+    console.log('This is in the catch: ' + message)
+});

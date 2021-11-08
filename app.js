c14ab39d0b7b99093bc3ded91b0518e37847126c
Kevin Wong <kevingywong@gmail.com>
2021-11-08 14:29:10 -0500

    updated code

--- app.js
+++ app.js
@@ -1,6 +1,31 @@
 console.log("Hello World!\n==========\n");
 
 // Exercise 1 Section
+/*
+function watchTurorialCallback(callback, errorCallback) {
+  let userLeft = false;
+  let userWatchingLiveStream = true;
+
+  if (userLeft) {
+    errorCallback({
+      name: "User Left",
+      message: ":(",
+    });
+  } else if (userWatchingLiveStream) {
+    callback("Thumbs up and Subscribe");
+  }
+}
+
+watchTurorialCallback(
+  (message) => {
+    console.log(message);
+  },
+  (error) => {
+    console.log(error.name + " " + error.message);
+  }
+);
+*/
+
 console.log("EXERCISE 1:\n==========\n");
 let isWatching = new Promise((resolve, reject) => {
     let userWatchingLiveStream = true;
@@ -12,7 +37,7 @@ let isWatching = new Promise((resolve, reject) => {
 });
 
 isWatching.then((message) => {
-    console.log('This is in the then : ' + message)
+    console.log('This is in the then: ' + message)
 }).catch((message) => {
     console.log('This is in the catch: ' + message)
 });
